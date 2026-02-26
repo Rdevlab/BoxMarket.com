bed4ae7366c29362aa8436df5550f41f2de6cbd0
Rohullah Ahmadi <rohullahahmadi531@gmail.com>
2026-02-27 03:32:31 +0430

    edit 2

--- script.js
+++ script.js
@@ -2,5 +2,6 @@ const mobileMenu = document.querySelector(".mobileMenu");
 const showMenuBtn = document.querySelector(".mobileMenuBtn");
 showMenuBtn.addEventListener("click", () => {
   mobileMenu.classList.toggle("transform-[translatey(-150%)]");
+  mobileMenu.classList.toggle("h-screen");
   showMenuBtn.classList.toggle("rotate-180");
 });
